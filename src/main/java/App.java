27df5a00863ea648d308212459ab659c6af1c0c9
Peter Beach <peterbeach2@gmail.com>
2016-04-29 09:24:07 -0700

    list out words on index.vtl. Word class fully integrated.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -11,9 +12,27 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("words", request.session().attribute("words"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/wordInput", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+
+    ArrayList<Word> words = request.session().attribute("words");
+    if (words == null) {
+      words = new ArrayList<Word>();
+      request.session().attribute("words", words);
+    }
+
+    String word = request.queryParams("word");
+    Word newWord = new Word(word);
+    words.add(newWord);
+
+    model.put("template", "templates/success.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,8 +3,9 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import java.util.ArrayList;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -21,5 +22,35 @@ public class AppTest extends FluentTest {
   public void rootTest() {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Words");
+  }
+
+  @Test
+  public void WordIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    fill("#word").with("Hi");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your word has been added to the dictionary!");
+  }
+
+  @Test
+  public void WordIsDisplayedOnIndexTest() {
+    goTo("http://localhost:4567/");
+    fill("#word").with("Hello");
+    submit(".btn");
+    click("a", withText("Return to previous page"));
+    assertThat(pageSource()).contains("Hello");
+  }
+
+  @Test
+  public void MultipleWordsAreDisplayedOnIndexTest() {
+    goTo("http://localhost:4567/");
+    fill("#word").with("Hi");
+    submit(".btn");
+    click("a", withText("Return to previous page"));
+    fill("#word").with("There");
+    submit(".btn");
+    click("a", withText("Return to previous page"));
+    assertThat(pageSource()).contains("Hi");
+    assertThat(pageSource()).contains("There");
   }
 }
